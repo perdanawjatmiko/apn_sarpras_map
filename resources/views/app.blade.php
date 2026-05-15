cfca25e48e5acfb74e52f61eafd0de7badfee3eb
Perdana Erda <perdanawjatmiko@gmail.com>
2026-05-15 10:40:53 +0700

    update logo kdkmp

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,16 +30,15 @@
             }
         </style>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" href="/logokdkmp.png" type="image/png">
+        <link rel="apple-touch-icon" href="/logokdkmp.png">
 
         @fonts
 
         @viteReactRefresh
         @vite(['resources/css/app.css', 'resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
         <x-inertia::head>
-            <title>{{ config('app.name', 'Laravel') }}</title>
+            <title>{{ config('app.name', 'APN SARPRAS MAP') }}</title>
         </x-inertia::head>
     </head>
     <body class="font-sans antialiased">
